b11b0c214aa0b92a9e2593bffc81de7043610a48
duneding <maj.dagostino@gmail.com>
2016-05-16 11:26:48 -0300

    update about me

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 var TelegramBot = require('node-telegram-bot-api'),
 	ip = require('ip'),
-  config = require('./config'); 
+  config = require('./config'),
+  multiline = require('multiline');
+
+var about_me = multiline(function(){/*
+Hola, ¿como estás?...
+Mi nombre es Hali!, la primer asistente de la UTN para personas de la UTN.
+Fui contruida por el Grupo de Proyecto Final G501 en el año 2016.
+Mi mision es ayudarte en tu carrera en todo lo que este a mi alcance.
+Para esto seria genial conocernos, y aprender mutuamente.
+Cuanto más cuentes conmigo más voy a mejorar y aprender.
+Muchas Gracias, y espero que esta relacion sea muy productiva para ambos.
+Por cierto venis muy bien en la carrera! Vamos por más. :)
+
+Me encuentro en estado de entrenamiento. En breve ya voy a ser más inteligente! :)
+Cuando te recibas vamos a tomar unas cervezas!!!
+
+*/});
 
 var TOKEN = config.token;
 var USER = config.user;
@@ -16,18 +32,16 @@ var opts = {
     }
   )};
 
-
 console.log("Server [" + ip.address() + "] listening...");
 
-
-bot.sendMessage(USER, 'How old are you?', opts)
+/*bot.sendMessage(USER, 'How old are you?', opts)
   .then(function (sended) {
     var chatId = sended.chat.id;
     var messageId = sended.message_id;
     bot.onReplyToMessage(chatId, messageId, function (message) {
       console.log('User is %s years old', message.text);
     });
-});
+});*/
 
 // Matches /echo [whatever]
 bot.onText(/\/echo (.+)/, function (msg, match) {
@@ -38,7 +52,8 @@ bot.onText(/\/echo (.+)/, function (msg, match) {
 
 // Any kind of message
 bot.on('message', function (msg) {
+  //console.log("Any kind of message");
   var chatId = msg.chat.id;
-  console.log(msg);
-  bot.sendMessage(chatId, "Hola!!");
+  //console.log(msg);
+  bot.sendMessage(chatId, about_me);
 });
