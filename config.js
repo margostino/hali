19b0489717794c2f449468da26b54cf545cb702f
duneding <maj.dagostino@gmail.com>
2016-05-17 22:54:51 -0300

    remove config

--- config.js
+++
@@ -1,7 +0,0 @@
-var config = {};
-
-config.token_tg = '219665776:AAEigXWrsa16CxeVqPWvhOoMolhUm7ADVyI';
-config.token_wit = 'FZH5RLDHPEIDWZB35ZIL2F4HG3KGNAZN';
-config.user = '211613276';
-
-module.exports = config;
