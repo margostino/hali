d17cd039d1730cdbaf01baf95b6ffe8989bd504e
duneding <maj.dagostino@gmail.com>
2016-05-22 23:55:39 -0300

    updates merge

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ var TelegramBot = require('node-telegram-bot-api'),
   log4js = require('log4js'),
   datetime = require('node-datetime'),
   weather = require('weather-js'),
-  jsStringEscape = require('js-string-escape'),
-  encoding = require("encoding"),
-  convertString = require("convert-string"),
-  S = require('string');
-
+  isEmoji = require('is-emoji-keyword'),
+  isSEmoji = require('is-standard-emoji'),
+  unicode = require("emoji-unicode-map"),
+  emoji = require("emoji-dictionary"),
+  convert = require('emojize').emojize,
+  hash = require('object-hash'),
+  should = require('should'),
+  google = require('google'),
+  random = require("random-js")(); // uses the nativeMath engine
 
 //LOGGER
 var logger = log4js.getLogger();
@@ -49,17 +53,26 @@ Dia a dia voy a gregando nuevos skills.
 Arriba esas palmas!! Clap clap!
 */});
 
-const NOT_STORY = 'What?!!, Estoy en pleno entrenamiento de tu idioma!, banca un rato o enseñame!!!';
+const NOT_STORY = multiline(function(){/*
+Estoy aprendiendo tu idioma aún. 
+Algunas preguntas o comentarios no los entiendo todavia. 
+En breve puedo responder ese mensaje.
+*/});
+
+const GOOGLE_IT = "https://www.google.com.ar/#q=";
 const START = 'Bienvenido!';
 const STOP = 'Hasta pronto!';
 const TOKEN_TG = config.token_tg;
 const TOKEN_WIT = config.token_wit;
 const USER = config.user;
-const SESSION = 'hali-session_test';
-const context = {};
 
+var session_wit = hash({
+                        app:'hali',
+                        env:'dev',
+                        datetime:datetime.create(Date.now()),
+                        random:random.integer(1, 9000)
+                      });
 var response = '';
-
 const firstEntityValue = (entities, entity) => {
   const val = entities && entities[entity] &&
     Array.isArray(entities[entity]) &&
@@ -73,11 +86,22 @@ const firstEntityValue = (entities, entity) => {
 };
 
 const getIntentValue = (entities, value) => {
-  const val = firstEntityValue(entities, 'intent');
-  return val === value ? val : null;
+  val = null;    
+  if (entities && entities["intent"] && 
+      Array.isArray(entities["intent"]) &&
+      entities["intent"].length > 0){
+
+    var intent = _.findWhere(entities["intent"], {value:value});  
+    if (!_.isUndefined(intent))
+      val = intent.value;
+  }
+
+  //const val = firstEntityValue(entities, 'intent');
+  //return val === value ? val : null;
+  return val;
 };
 
-const updateContext = (context, entities, value) => {
+/*const updateContext = (context, entities, value) => {   
   const context_value = getIntentValue(entities, value);
   if (context_value)
       context[value] = context_value;      
@@ -85,31 +109,78 @@ const updateContext = (context, entities, value) => {
       delete context[value];
 
   return context;
-};
+};*/
 
+var contexts = [
+  ['bye'],['greeting'],['greeting','greeting_how'],
+  ['thanks'],['weather'],['where'],['more_info'],['greeting_how'],
+  ['hali_birth'],['hali_name'],['hali_lang'],['get_datetime'],
+  ['hali_skills'],['hali_arq'],['hali_age'],['hali_private'],
+  ['hali_sex'],['hali_color'],['sent_positivo','sent_negativo'],
+  ['who'],['sent_negativo'],['sent_positivo'],['insulto'],
+  ['aula','query_availability'],['search_departamento','where'],
+  ['especialidad'],['hali_activity'],['about_it'],['google_it'],
+  ['about_it','hali_activity'],['search_departamento', 'where','especialidad']
+];
+
+function matchContext(context, message){
+  var current = _.keys(context);
+  var hit = null;
+  
+  _.each(contexts, function(c){    
+    var diff = _.difference(current, c);
+    if (current.length==c.length && _.isEmpty(diff)){      
+      hit = c;       
+      return;
+    }
+  });
+  
+  if (!hit){
+    context = {};
+    context["not_story"] = NOT_STORY;
+  }
+
+  context["msg_request"] = message;  
+  return context;
+}
+
+function updateContext(context, entities){  
+  var keys = _.keys(entities);
+  _.each(keys, function(entity){      
+      _.each(entities[entity], function(values){          
+        if (entity=="intent")
+          context[values.value] = values.value;
+        else
+          context[entity] = values.value;          
+      });
+  });
+  return context;  
+}
+
+/*function updatePreContext(context){
+  _.each(_.keys(context), function(k){
+    if (k!='msg_request')
+      context[k] = pre_context[k];
+  });
+  
+  return context;
+}*/
 
 const actions = {
-  say(sessionId, context, message, cb) {
-    
+  say(sessionId, context, message, cb) {    
     if (_.has(context, "not_story"))
         logger.info(context["msg_request"]);
-
     response = message;
     //console.log(message);
     cb();
   },
   merge(sessionId, context, entities, message, cb) {  
-    const aula = firstEntityValue(entities, 'aula');
-    if (aula)
-      context.aula = aula;
-    else
-      delete context.aula;
-    
-    context = updateContext(context, entities, "search_departamento");
+
+    /*context = updateContext(context, entities, "search_departamento");
     context = updateContext(context, entities, "who");
     context = updateContext(context, entities, "insulto");
-    context = updateContext(context, entities, "greeting");
-    context = updateContext(context, entities, "greeting_how");
+    context = updateContext(context, entities, "greeting");        
+    context = updateContext(context, entities, "greeting_how");    
     context = updateContext(context, entities, "sent_positivo");
     context = updateContext(context, entities, "sent_negativo");
     context = updateContext(context, entities, "aula");
@@ -124,21 +195,21 @@ const actions = {
     context = updateContext(context, entities, "hali_name");
     context = updateContext(context, entities, "hali_birth");
     context = updateContext(context, entities, "more_info");
-    context = updateContext(context, entities, "greeting_bye");
+    context = updateContext(context, entities, "bye");
     context = updateContext(context, entities, "hali_home");
     context = updateContext(context, entities, "weather");
     context = updateContext(context, entities, "thanks");
+    context = updateContext(context, entities, "emoji");*/
 
-    if (_.keys(context).length==0)
-      context["not_story"] = NOT_STORY; 
-
-    context["msg_request"] = message;
-    console.log("context:  " + JSON.stringify(context));
-    console.log("entities:  " + JSON.stringify(entities));
-
+    console.log("Previous Context:  " + JSON.stringify(context));
+    context = updateContext(context, entities);            
+    context = matchContext(context, message);
+    console.log("New Context:  " + JSON.stringify(context));
+    console.log("Entities:  " + JSON.stringify(entities));    
     cb(context);
   },
   error(sessionId, context, error) {
+    console.log('Not Story!');
     console.log(error.message);
   },
   ['get-availability'](sessionId, context, cb) {
@@ -172,7 +243,11 @@ const actions = {
     var dt_formatted = dt.format('d f Y H:M:S');
     context.datetime = dt_formatted;
     cb(context);
-  },     
+  }, 
+  ['google-it'](sessionId, context, cb){    
+    context.results=GOOGLE_IT;
+    cb(context);    
+  },    
   ['get-weather'](sessionId, context, cb) {
     weather.find({search: 'Capital Federal, Buenos Aires', lang: 'es-ES', degreeType: 'C'}, function(err, result) {
       if(err) console.log(err);
@@ -206,9 +281,17 @@ var opts = {
   )};
 
 console.log("Server [" + ip.address() + "] listening...");
-//const logger = new Logger(levels.DEBUG);
-//const CLIENT = new Wit(TOKEN_WIT, actions, logger);
+console.log("Session: " + session_wit);
+
+//const logger2 = new Logger(levels.DEBUG);
+//const CLIENT = new Wit(TOKEN_WIT, actions, logger2);
 const CLIENT = new Wit(TOKEN_WIT, actions);
+
+var regEx = new RegExp('(<span class=\"emoji [_]([0-9]*[a-zA-Z]*[0-9]*)*\"><\/span>)*','g');
+  
+/*console.log(unicode.get(message));
+console.log(isSEmoji(message));*/
+//console.log(unicode.emoji);
 
 // Any kind of message
 bot.on('message', function (msg) {
@@ -221,19 +304,44 @@ bot.on('message', function (msg) {
   console.log("From: " + from);
   console.log("Chat ID: " + chatId);
   
-  if (message == '/start')
-    bot.sendMessage(chatId, START);
-  else if (message == '/stop')
-    bot.sendMessage(chatId, STOP);
-  else
-    CLIENT.runActions(SESSION, message, context, (error, context1) => {
-    if (error) {
-      console.log('Oops! Got an error: ' + error);
-    } else {
-      bot.sendMessage(chatId, response);
-    }
-  });
+  if(isSEmoji(message))
+    bot.sendMessage(chatId, message);
+  else{
+    message_sanitized = sanitizeMessage(message);
+
+    if (message_sanitized)
+      runActionsWit(chatId, message_sanitized);
+    else
+      bot.sendMessage(chatId, message);
+
+  }
 });
+
+function sanitizeMessage(message){
+  var message_sanitized = message;
+  var message_converted = convert(message);
+  var span = message_converted.match(regEx); 
+  span = _.filter(span, function(e){return e!=''});
+  if (span.length>0){              
+      for (var i = 0; i<span.length; i++)                
+        message_converted = message_converted.replace(span[i], "").trim();                                
+      message_sanitized = message_converted;
+  }  
+
+  return message_sanitized;    
+}
+
+function runActionsWit(chatId, message){
+  console.log("Ejecuta Wit.ai");
+  CLIENT.runActions(session_wit, message, context, (error, context1) => {
+        if (error) {
+          console.log('Oops! Got an error: ' + error);          
+          bot.sendMessage(chatId, NOT_STORY);  
+        } else {
+          bot.sendMessage(chatId, response);
+        }
+  });
+}
 
 function configureLogs(logger){
   
@@ -247,4 +355,6 @@ function configureLogs(logger){
     ]
   });
 }
-//CLIENT.interactive();
+
+CLIENT.interactive();
+
